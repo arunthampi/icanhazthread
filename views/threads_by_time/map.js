09744f6d3804826904df6e325d641970253b9b69
arunthampi <arun.thampi@gmail.com>
2009-08-16 11:37:30 +0800

    Update view

--- views/threads_by_time/map.js
+++ views/threads_by_time/map.js
@@ -1,6 +1,6 @@
 function(doc) {
   // This view gives all threads which are in a specific time-frame
-  if(doc['couchrest-type'] == 'HazChatThread') {
-    emit(doc.updated_at, doc);
+  if(doc.type == 'thread') {
+    emit(doc.time_created, doc);
   }
 }
