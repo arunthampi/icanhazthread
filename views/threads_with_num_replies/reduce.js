42769da6968d5dbf543296502c98d2c7cd29c305
arunthampi <arun.thampi@gmail.com>
2009-08-19 12:32:26 +0800

    Fix bug in threads_with_num_replies's reduce function: If timestamp of result is < than the input, only then assign. Writing map/reduce functions while sleepy is injurious to health

--- views/threads_with_num_replies/reduce.js
+++ views/threads_with_num_replies/reduce.js
@@ -1,18 +1,29 @@
 function(keys, values, rereduce) {
-  var result = { num_replies: 0, last_reply: 0, text: "", time_created: 0 };
+  var result = { num_replies: 0, last_reply: 0, text: "", time_created: 0, screen_name: "" };
 
   if(rereduce) {
     for(idx = 0; idx < values.length; idx++) {
       result.num_replies += values[idx].num_replies;
-      result.text = values[idx].text;
-      result.screen_name = values[idx].screen_name;
-      result.time_created = values[idx].time_created;
 
-      if(result.last_reply == 0) {
-        result.last_reply = values[idx].time_created;
-      } else {
-        if(result.last_reply > values[idx].time_created) {
-          result.last_reply = values[idx].time_created;
+      if(values[idx].text != "") {
+        result.text = values[idx].text;
+      }
+    
+      if(values[idx].screen_name != "") {
+        result.screen_name = values[idx].screen_name;
+      }
+      
+      if(values[idx].time_created != 0) {
+        result.time_created = values[idx].time_created;
+      }
+
+      if(values[idx].last_reply != 0) {
+        if(result.last_reply == 0) {
+          result.last_reply = values[idx].last_reply;
+        } else {
+          if(result.last_reply < values[idx].last_reply) {
+            result.last_reply = values[idx].last_reply;
+          }
         }
       }
     }
@@ -30,7 +41,7 @@ function(keys, values, rereduce) {
         if(result.last_reply == 0) {
           result.last_reply = values[idx].time_created;
         } else {
-          if(result.last_reply > values[idx].time_created) {
+          if(result.last_reply < values[idx].time_created) {
             result.last_reply = values[idx].time_created;
           }
         }
