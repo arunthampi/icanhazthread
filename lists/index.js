7b129f57feee10b81eda93d40e362770c5c9c8f1
arunthampi <arun.thampi@gmail.com>
2009-08-24 07:32:51 +0800

    Fix date, and don't assume localhost like a dork

--- lists/index.js
+++ lists/index.js
@@ -5,36 +5,41 @@ function(head, req) {
   // The URL to access this is at:
   // http://localhost:5984/hazchat/_design/icanhazthread/_list/index/threads_with_num_replies?format=atom&descending=true&group_level=1
   
+  function makeAbsolute(req, path) {
+    return 'http://' + req.headers.Host + path;
+  };
+  
   provides("atom", function() {
     var row = getRow();
     
     // generate the feed header
     var feedHeader = Atom.header({
-      updated : (row ? new Date(row.value.time_created) : new Date()),
+      updated : (row ? new Date(row.value.time_created * 1000) : new Date()),
       title : 'Latest threads from i.canhazthread.com',
-      feed_id : "http://localhost:3000",
-      feed_link : "http://localhost:3000?format=atom"
+      feed_id : makeAbsolute(req, "/"),
+      feed_link : makeAbsolute(req, "?format=atom")
     });
     
     send(feedHeader);
     
     if(row) {
       do {
-        var created_time = new Date(row.value.time_created);
-        var content = "Thread created at " +  created_time + ".";
+        var created_time = new Date(row.value.time_created * 1000);
+        var content = "This thread was started at " +  created_time + " by <a href=\"http://twitter.com/" +
+                      row.value.screen_name + ">" + row.value.screen_name + "</a>.";
 
         if(row.value.num_replies > 0) {
           content += " This thread has " + row.value.num_replies + " replies and the last reply was made at " +
-                      new Date(row.value.last_reply);
+                      new Date(row.value.last_reply * 1000);
         } else {
-          content += "No one has replied to this thread yet.";
+          content += " No one has replied to this thread yet.";
         }
 
         var feedEntry = Atom.entry({
-          entry_id : "http://localhost:3000/t/" + row.value.time_created + "-" + row.value.screen_name,
+          entry_id : makeAbsolute(req, "/t/" + row.value.time_created + "-" + row.value.screen_name),
           title : row.value.text,
           content : content,
-          updated : new Date(row.value.time_created).rfc3339(),
+          updated : created_time.rfc3339(),
           author : "@" + row.value.screen_name
         });
         // send the entry to client
